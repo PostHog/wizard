da28f68b9539d21f629d077d7e0d877ab60693b0
JonathanLab <jonathanmieloo@gmail.com>
2025-07-16 16:55:18 +0200

    remove console

--- e2e-tests/global-teardown.ts
+++ e2e-tests/global-teardown.ts
@@ -1,9 +1,6 @@
 import { fixtureTracker } from './mocks/fixture-tracker';
 
 export default function globalTeardown() {
-  // eslint-disable-next-line no-console
-  console.log('Global teardown: Cleaning up unused fixtures...');
-
   // Clean up unused fixtures after all tests complete
   if (process.env.CLEANUP_UNUSED_FIXTURES !== 'false') {
     fixtureTracker.cleanupUnusedFixtures();
@@ -14,8 +11,5 @@ export default function globalTeardown() {
     console.log(
       `Fixture usage stats: ${stats.usedFixtures}/${stats.existingFixtures} fixtures used, ${stats.unusedFixtures} deleted`,
     );
-  } else {
-    // eslint-disable-next-line no-console
-    console.log('Fixture cleanup disabled by CLEANUP_UNUSED_FIXTURES=false');
   }
 }
