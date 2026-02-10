e87a9030fc96cf695d7bac373743bb58745b5d47
Edwin Lim <edwin@posthog.com>
2026-02-09 21:56:09 -0500

    feat: react native and expo support (#268)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -9,6 +9,7 @@ export enum Integration {
   reactRouter = 'react-router',
   tanstackStart = 'tanstack-start',
   tanstackRouter = 'tanstack-router',
+  reactNative = 'react-native',
   angular = 'angular',
   astro = 'astro',
   django = 'django',
