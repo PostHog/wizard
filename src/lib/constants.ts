12a2a295be31720a291131a9bb4faba7a244c948
Sarah Sanders <88458517+sarahxsanders@users.noreply.github.com>
2026-02-25 11:02:30 -0500

    feat: add Node.js skill (#283)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -24,6 +24,7 @@ export enum Integration {
   // Language fallbacks
   python = 'python',
   ruby = 'ruby',
+  javascriptNode = 'javascript_node',
   javascript_web = 'javascript_web',
 }
 export interface Args {
